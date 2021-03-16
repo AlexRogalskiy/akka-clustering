d2214e2e97d1ad4aed03ba9c08efb1c30becf288
Vadim <vadopolski@gmail.com>
2021-03-17 00:40:52 +0300

    Added scalafmt

--- src/main/scala/metro/MetroStation.scala
+++ src/main/scala/metro/MetroStation.scala
@@ -22,7 +22,7 @@ class Turnstile(validator: ActorRef) extends Actor with ActorLogging {
     case o: TroykaCard =>
       log.info(s"$validator")
       validator ! EntryAttempt(o, new Date)
-    case EntryAccepted => log.info("GREEN: пропуск оформлен, проходите пожалуйста")
+    case EntryAccepted         => log.info("GREEN: пропуск оформлен, проходите пожалуйста")
     case EntryRejected(reason) => log.info(s"RED: $reason")
   }
 }
@@ -42,22 +42,24 @@ class TroykaCovidPassValidator extends Actor with ActorLogging {
 }
 
 object TurnstileSettings {
-  val numberOfShards = 3
+  val numberOfShards   = 3
   val numberOfEntities = 30
 
   /**
-   * Создаем уникальный идентификатор ентинти
-   * */
+    * Создаем уникальный идентификатор ентинти
+    */
   val extractEntityId: ShardRegion.ExtractEntityId = {
     case attempt @ EntryAttempt(TroykaCard(cardId, _), _) =>
       val entityId = cardId.hashCode.abs % numberOfEntities
-      println(s"!!!!!!!!!!!!!! EXTRACT ENTITY ID for card # ${attempt.troykaCard.id} to entity ID ${entityId}")
+      println(
+        s"!!!!!!!!!!!!!! EXTRACT ENTITY ID for card # ${attempt.troykaCard.id} to entity ID ${entityId}"
+      )
       (entityId.toString, attempt)
   }
 
   /**
-   * Cоздаем уникальный идентификатор группы (шарда) для каждого сообщения. Гарантия отсутствия дубликатов
-   * */
+    * Cоздаем уникальный идентификатор группы (шарда) для каждого сообщения. Гарантия отсутствия дубликатов
+    */
   val extractShardId: ShardRegion.ExtractShardId = {
     case EntryAttempt(TroykaCard(cardId, _), _) =>
       val shardId = cardId.hashCode.abs % numberOfShards
@@ -66,18 +68,17 @@ object TurnstileSettings {
   }
 }
 
-
 /**
- * Обратите внимание, что MetroStation не запускает акторы TroykaCovidPassValidator, в отличие от singleton-версии Shoppers.
- * Модуль ClusterSharding автоматически запустит TroykaCovidPassValidator при попытке переслать ему команду.
- * Он извлечет идентификаторы shopperId и shardId из сообщения и создаст соответствующий экземпляр TroykaCovidPassValidator,
- * используя entryProps, переданный ранее. Все последующие команды будут передаваться этому актору TroykaCovidPassValidator.
- * */
+  * Обратите внимание, что MetroStation не запускает акторы TroykaCovidPassValidator, в отличие от singleton-версии Shoppers.
+  * Модуль ClusterSharding автоматически запустит TroykaCovidPassValidator при попытке переслать ему команду.
+  * Он извлечет идентификаторы shopperId и shardId из сообщения и создаст соответствующий экземпляр TroykaCovidPassValidator,
+  * используя entryProps, переданный ранее. Все последующие команды будут передаваться этому актору TroykaCovidPassValidator.
+  */
 
 class MetroStation(port: Int, amountOfTurnstiles: Int) extends App {
 
-  val config = ConfigFactory.parseString(
-    s"""
+  val config = ConfigFactory
+    .parseString(s"""
        |akka.remote.artery.canonical.port = $port
      """.stripMargin)
     .withFallback(ConfigFactory.load("clusterShardingExample.conf"))
@@ -93,7 +94,7 @@ class MetroStation(port: Int, amountOfTurnstiles: Int) extends App {
   )
 
   val turnstiles: immutable.Seq[ActorRef] = (1 to amountOfTurnstiles)
-    .map{x =>
+    .map { x =>
       println(s"Before Starting actor of turnstiles #${x}")
       system.actorOf(Turnstile.props(validatorShardRegionRef))
     }
@@ -101,7 +102,7 @@ class MetroStation(port: Int, amountOfTurnstiles: Int) extends App {
   Thread.sleep(10000)
   for (_ <- 1 to 1000) {
     val randomTurnstileIndex = Random.nextInt(amountOfTurnstiles)
-    val randomTurnstile = turnstiles(randomTurnstileIndex)
+    val randomTurnstile      = turnstiles(randomTurnstileIndex)
 
     randomTurnstile ! TroykaCard(UUID.randomUUID().toString, Random.nextBoolean())
     Thread.sleep(200)
@@ -109,5 +110,5 @@ class MetroStation(port: Int, amountOfTurnstiles: Int) extends App {
 }
 
 object ChistyePrudy extends MetroStation(2551, 10)
-object Lubyanka extends MetroStation(2561, 5)
-object OkhotnyRyad extends MetroStation(2571, 15)
+object Lubyanka     extends MetroStation(2561, 5)
+object OkhotnyRyad  extends MetroStation(2571, 15)
